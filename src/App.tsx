7cbd1ee29dd721406f5993174e4a9b499e98eaea
corentinthebaud <corentin.thebaud44@gmail.com>
2025-04-07 17:35:42 +0200

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
 
 import './App.css'
+import Card from './components/Card/Card'
+
+const arrayOfPlanete = [
+  {
+    name: "Mercure",
+    imgSrc: "/planete/mercure.webp"
+  },
+  {
+    name: "Vénus",
+    imgSrc: "public/planete/venus.webp"
+  },
+  {
+    name: "Terre",
+    imgSrc: "public/planete/terre.jpg"
+  },
+  {
+    name: "Mars",
+    imgSrc: "public/planete/mars.webp"
+  },
+  {
+    name: "Jupiter",
+    imgSrc: "public/planete/Jupiter.webp"
+  },
+  {
+    name: "Saturne",
+    imgSrc: "public/planete/Saturne.avif"
+  },
+  {
+    name: "Uranus",
+    imgSrc: "public/planete/uranus.webp"
+  },
+  {
+    name: "Neptune",
+    imgSrc: "public/planete/Neptune.webp"
+  },
+  
+]
 
 function App() {
 
 
   return (
     <>
-      <h1>bonjour</h1>
+    <main>
+    <h1>Les planètes</h1>
+    <section className='planete'>
+    {arrayOfPlanete.map((element) => {
+        return (
+          <Card name={element.name} imgSrc={element.imgSrc} />
+        )
+      })}
+    </section>
+
+    </main>
+      
     </>
   )
 }
